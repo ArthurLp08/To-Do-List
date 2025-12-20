219f79b74f755d517b0df515e45d62a37c61bc3e
Arthur Lopes <josearthurlda@gmail.com>
2025-12-20 02:17:30 -0300

    Started the input button

---
+++ src/components/input.jsx
@@ -0,0 +1,9 @@
+function Input(){
+    return(
+        <>
+            <input className="bg-[#BDE0FE] w-1/2 h-[57px] indent-4 rounded-3xl place placeholder:text-black" type="text" placeholder="Type some task you have to do" />
+        </>
+    )
+}
+
+export default Input
