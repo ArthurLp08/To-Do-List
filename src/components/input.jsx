411a8592b4aa87b550d6bd74c19ea32f1f40d56b
Arthur Lopes <josearthurlda@gmail.com>
2025-12-20 03:18:25 -0300

    ToDos style finished and localstorage implemented

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -1,12 +1,49 @@
+import { useState, useEffect } from "react"
 import addIcon from "../assets/addIcon.svg"
+import deleteIcon from "../assets/deleteIcon.svg"
 
 function Input(){
+    const [task, setTask] = useState("")
+    const [todos, setTodos] = useState([])
+
+    const handleChange = (e) => {
+        setTask(e.target.value);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (task.trim()){
+            setTodos([...todos, task]);
+            setTask("");
+        }
+    }
+
+    useEffect(() => {
+        if (localStorage.getItem("ToDos") == null) return;
+        setTodos(JSON.parse(localStorage.getItem("ToDos")));
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem("ToDos", JSON.stringify(todos));
+    }, [todos]);
+
     return(
-        <div className="relative w-1/2">
-            <input className="bg-[#BDE0FE] w-full h-[57px] indent-4 rounded-3xl place placeholder:text-black" type="text" placeholder="Type some task you have to do" />
-            <button className="bg-[#CDB4DB] p-3 rounded-full absolute right-1 top-[50%] translate-y-[-50%]" type="submit"><img src={ addIcon } alt="" /></button>
-        </div>
+        <>
+        <form onSubmit={handleSubmit} className="relative w-[50%] border-[#BDE0FE] border-b-4 flex items-center">
+            <input onChange={handleChange} value={task} className="bg-[#BDE0FE] m-4  w-full h-[57px] indent-4 rounded-3xl placeholder:text-black" type="text" placeholder="Type some task you have to do" />
+            <button className="bg-[#CDB4DB] p-3 rounded-full absolute right-6 top-[50%] translate-y-[-50%]" type="submit"><img src={ addIcon } alt="" /></button>
+        </form>
+        <ul className="flex flex-col w-[50%] p-3">
+        {todos.map((todo, index) => (
+          <li className=" relative bg-[#BDE0FE] mt-2 w-full h-[57px] indent-4 rounded-3xl flex items-center" key={index}> {todo} <button className="bg-[#CDB4DB] p-3 rounded-full absolute right-3 top-[50%] translate-y-[-50%]" type="submit"><img src={ deleteIcon } alt="" /></button> </li>
+          
+        ))}
+        </ul>
+        </>
     )
+
+
 }
 
 export default Input
